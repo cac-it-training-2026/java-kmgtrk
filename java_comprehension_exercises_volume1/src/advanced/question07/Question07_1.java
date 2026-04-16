8d20eae2f12c5fb6e7c18cae0c269dcd54dabeeb
kamagata <kamagata@C010423140.zipangu.cac.co.jp>
2026-04-16 10:24:58 +0900

    完了しました。

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
@@ -7,6 +7,15 @@ public class Question07_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		int price = 120;
+		String item = "ノート";
+		System.out.println(price);
+		System.out.println(item);
+
+		price = 200;
+		item = "鉛筆";
+		System.out.println(price);
+		System.out.println(item);
 
 	}
 
